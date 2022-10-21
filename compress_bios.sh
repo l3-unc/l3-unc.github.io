2620e0b97810ae2f22ebbe4fba20353d22fd8ba0
rrmenon10 <rockmenon@icloud.com>
2022-10-21 14:48:23 -0400

    updating images to be uniformly sized

---
+++ compress_bios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# NOTE: Perhaps one day we can do this with GitHub Actions,
+# https://github.com/marketplace/actions/image-actions looks promising but doesn't
+# support resizeing yet and overwrites the existing image.
+
+# {jpg,png,jpeg,webp}
+
+rm -rf "images/bios_compressed"
+mkdir -p "images/bios_compressed"
+
+for filename in "images/teampic"/*.{jpg,png,jpeg}
+do
+    basename=$(basename ${filename%.*})
+    echo "$basename"
+    convert -define jpeg:size=200x200 $filename -thumbnail '195x195>' -quality 85 \
+        -background white -gravity center -extent 200x200 "images/bios_compressed/$basename.jpg"
+done
